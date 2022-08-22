e9f1cf01a262012da6e88f0fa488582183570d0b
Anastasiyzaranko <98044648+Anastasiyzaranko@users.noreply.github.com>
2022-08-22 16:02:38 +0300

    семинар 5, задача 38

---
+++ Task5_38/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+void FillAray(int[] arr)// ЗАПОЛНЯЕМ МАССИВ
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArr(int[] arr)// распечатали массив
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{arr[i]} ");
+    }
+}
+
+int[] array = new int[6];
+FillAray(array);
+PrintArr(array);
+
+int min = 99;
+int max = 0;
+int rez = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+
+    if (array[i] < min) min = array[i];
+
+    rez = max - min;
+}
+Console.WriteLine($" максимальное значение {max}");
+
+Console.WriteLine($" минимальное значение {min}");
+
+
+Console.WriteLine($" разница между максимальным  и минимальным равна {max}-{min}= {rez}");
